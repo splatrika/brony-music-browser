ea7bbc2d51a75c8f1adf78e1d9f58051ab32bfe1
daniilbelov <tseflutty@yandex.ru>
2022-10-04 19:04:44 +0300

    Add album tests

---
+++ Server/Tests/Unit/Core/Entities/AlbumTests.cs
@@ -0,0 +1,118 @@
+﻿using Splatrika.BronyMusicBrowser.Core.Entities.AlbumAggregate;
+using Xunit;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Unit.Core.Entities;
+
+public class AlbumTests
+{
+    [Fact]
+    public void Create()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+
+        Assert.Equal(10, album.Id);
+        Assert.Equal("Album1", album.Title);
+        Assert.Equal(1, album.Songs.Count);
+        Assert.Equal(0, album.Artists.Count);
+        Assert.Equal(10, album.Songs.ElementAt(0).AlbumId);
+        Assert.Equal(2, album.Songs.ElementAt(0).SongId);
+    }
+
+
+    [Fact]
+    public void AddNewSong()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+        album.AddSong(4);
+
+        Assert.Equal(2, album.Songs.Count);
+        Assert.Equal(10, album.Songs.ElementAt(1).AlbumId);
+        Assert.Equal(4, album.Songs.ElementAt(1).SongId);
+    }
+
+    [Fact]
+    public void AddSongThatAlreadyInAlbum()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+
+        Assert.Throws<InvalidOperationException>(
+            () => album.AddSong(2));
+    }
+
+
+    [Fact]
+    public void RemoveSongWhenCountMoreThanOne()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+        album.AddSong(3);
+        album.RemoveSong(2);
+
+        Assert.Equal(1, album.Songs.Count);
+        Assert.Equal(3, album.Songs.First().SongId);
+    }
+
+
+    [Fact]
+    public void RemoveSongWhenCountIsOne()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+
+        Assert.Throws<InvalidOperationException>(
+            () => album.RemoveSong(2));
+    }
+
+
+    [Fact]
+    public void RemoveNonexistentSong()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+        album.AddSong(12);
+
+        Assert.Throws<InvalidOperationException>(
+            () => album.RemoveSong(33));
+    }
+
+
+    [Fact]
+    public void UpdateDetails()
+    {
+        var album = new Album(id: 10, firstSongId: 2, title: "Album1");
+        album.UpdateDetails("Album2");
+
+        Assert.Equal("Album2", album.Title);
+    }
+
+
+    [Fact]
+    public void AddArtist()
+    {
+        var album = new Album(id: 11, firstSongId: 2, title: "Album1");
+        album.AddArtist(12);
+
+        Assert.Equal(1, album.Artists.Count);
+        Assert.Equal(11, album.Artists.First().AlbumId);
+        Assert.Equal(12, album.Artists.First().ArtistId);
+    }
+
+
+    [Fact]
+    public void RemoveArtist()
+    {
+        var album = new Album(id: 11, firstSongId: 2, title: "Album1");
+        album.AddArtist(12);
+        album.RemoveArtist(12);
+
+        Assert.Equal(0, album.Artists.Count);
+    }
+
+
+    [Fact]
+    public void RemoveNonexistentArtist()
+    {
+        var album = new Album(id: 11, firstSongId: 2, title: "Album1");
+
+        Assert.Throws<InvalidOperationException>(
+            () => album.RemoveArtist(2));
+    }
+}
+
