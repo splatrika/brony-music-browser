6cafc4f6ec59ec0004300cb4b82f2795ca7be69c
daniilbelov <tseflutty@yandex.ru>
2022-10-05 21:10:09 +0300

    Add list guard

---
+++ Server/Source/Core/ListGuard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Splatrika.BronyMusicBrowser.Core;
+
+public static class ListGuard
+{
+    public static void CannotBeEmptyAfterRemove(ICollection list)
+    {
+        if (list.Count == 1)
+        {
+            throw new InvalidOperationException
+                ("Unable to remove item. List cannot be empty");
+        }
+    }
+
+
+    public static void CannotRemoveNonexistent<T>(ICollection<T> list, T item)
+    {
+        if (!list.Contains(item))
+        {
+            throw new InvalidOperationException
+                ("Unable to remove item because this item isnt't in list");
+        }
+    }
+
+
+    public static void CannotAddDublicate<T>(ICollection<T> list, T adding)
+    {
+        if (list.Contains(adding))
+        {
+            throw new InvalidOperationException
+                ("Unable to add item. List cannot contains dublicates");
+        }
+    }
+}
+
