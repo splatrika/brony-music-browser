0ad51a0a917c8a85c5f8554e5a8de7c46d9827b3
daniilbelov <tseflutty@yandex.ru>
2022-10-07 20:00:07 +0300

    Update dependencies extensions: add crud repositories

--- Server/Source/Infrastructure/Dependencies.cs
+++ Server/Source/Infrastructure/Dependencies.cs
@@ -2,6 +2,15 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Splatrika.BronyMusicBrowser.Infrastructure.Data;
+using Splatrika.BronyMusicBrowser.Core.Interfaces;
+using Splatrika.BronyMusicBrowser.Core.Projections;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data.Repositories.ReadOnly;
+using Splatrika.BronyMusicBrowser.Core.CreateArgs;
+using Splatrika.BronyMusicBrowser.Core.Entities.AlbumAggregate;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data.Repositories;
+using Splatrika.BronyMusicBrowser.Core.Entities;
+using Splatrika.BronyMusicBrowser.Core;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data.Filters;
 
 namespace Splatrika.BronyMusicBrowser.Infrastructure;
 
@@ -19,6 +28,22 @@ public static class Dependencies
         }
         services.AddDbContext<BrowserContext>(options =>
             options.UseSqlServer(connectionString));
+    }
+
+
+    public static void AddCrudRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<
+            ICrudRepository<Artist, ArtistCreateArgs>, ArtistCrudRepository>();
+        services.AddScoped<
+            ICrudRepository<Character, CharacterCreateArgs>,
+            CharacterCrudRepository>();
+        services.AddScoped<
+            ICrudRepository<Genre, GenreCreateArgs>, GenreCrudRepository>();
+        services.AddScoped<
+            ICrudRepository<Song, SongCreateArgs>, SongCrudRepository>();
+        services.AddScoped<
+            ICrudRepository<Album, AlbumCreateArgs>, AlbumCrudRepository>();
     }
 }
 
