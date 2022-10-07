08392eb80ebcc41ba082cfc5588427ec5d5919f3
daniilbelov <tseflutty@yandex.ru>
2022-10-07 19:33:36 +0300

    Add assert extensions

---
+++ Server/Tests/Unit/Extensions/AssertExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Unit.Extensions;
+
+public static class AssertExtensions
+{
+    public static void SequenceEqual<T>(
+        IEnumerable<T> excepted,
+        IEnumerable<T> actual)
+    {
+        var exceptedView = JsonConvert.SerializeObject(excepted);
+        var actualView = JsonConvert.SerializeObject(actual);
+        var message = $"Excepted: {exceptedView}\nActual: {actualView}";
+        Assert.True(excepted.SequenceEqual(actual), message);
+    }
+}
+
