ba18fc3cac54f8854addff2f2bf95db968850819
daniilbelov <tseflutty@yandex.ru>
2022-10-07 19:32:54 +0300

    Add builders for tests

---
+++ Server/Tests/Unit/Core/Builders/AlbumBuilder.cs
@@ -0,0 +1,73 @@
+﻿using Splatrika.BronyMusicBrowser.Core.Entities.AlbumAggregate;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Unit.Core.Builders;
+
+public class AlbumBuilder
+{
+    private string _title;
+    private string _cover;
+    private int _firstSong;
+    private List<int> _songs;
+    private List<int> _artists;
+
+
+    public AlbumBuilder()
+    {
+        _title = "Album";
+        _cover = "/cover.png";
+        _firstSong = 1;
+        _songs = new();
+        _artists = new();
+    }
+
+
+    public AlbumBuilder SetTitle(string value)
+    {
+        _title = value;
+        return this;
+    }
+
+
+    public AlbumBuilder SetCover(string value)
+    {
+        _cover = value;
+        return this;
+    }
+
+
+    public AlbumBuilder SetFirstSong(int id)
+    {
+        _firstSong = id;
+        return this;
+    }
+
+
+    public AlbumBuilder AddSong(int id)
+    {
+        _songs.Add(id);
+        return this;
+    }
+
+
+    public AlbumBuilder AddArtist(int id)
+    {
+        _artists.Add(id);
+        return this;
+    }
+
+
+    public Album Build()
+    {
+        var album = new Album(0, _firstSong, _title, _cover);
+        foreach (var song in _songs)
+        {
+            album.AddSong(song);
+        }
+        foreach (var artist in _artists)
+        {
+            album.AddArtist(artist);
+        }
+        return album;
+    }
+}
+
---
+++ Server/Tests/Unit/Core/Builders/SongBuilder.cs
@@ -0,0 +1,95 @@
+﻿using Splatrika.BronyMusicBrowser.Core.Entities;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Unit.Core.Builders;
+
+public class SongBuilder
+{
+    private string _title;
+    private string _cover;
+    private int _year;
+    private string _youTubeId;
+    private List<int> _artists;
+    private List<int> _genres;
+    private List<int> _characters;
+
+
+    public SongBuilder()
+    {
+        _title = "Just a song";
+        _cover = "/jus-a-cover.png";
+        _year = 0;
+        _youTubeId = "0";
+        _artists = new();
+        _genres = new();
+        _characters = new();
+    }
+
+
+    public SongBuilder SetTitle(string value)
+    {
+        _title = value;
+        return this;
+    }
+
+
+    public SongBuilder SetCover(string value)
+    {
+        _cover = value;
+        return this;
+    }
+
+
+    public SongBuilder SetYer(int value)
+    {
+        _year = value;
+        return this;
+    }
+
+
+    public SongBuilder SetYouTubeId(string value)
+    {
+        _youTubeId = value;
+        return this;
+    }
+
+
+    public SongBuilder AddArtist(int id)
+    {
+        _artists.Add(id);
+        return this;
+    }
+
+
+    public SongBuilder AddGenres(int id)
+    {
+        _genres.Add(id);
+        return this;
+    }
+
+
+    public SongBuilder AddCharacter(int id)
+    {
+        _characters.Add(id);
+        return this;
+    }
+
+
+    public Song Build()
+    {
+        var song = new Song(0, _title, _cover, _year, _youTubeId);
+        foreach (var artist in _artists)
+        {
+            song.AddArtist(artist);
+        }
+        foreach (var genre in _genres)
+        {
+            song.AddGenre(genre);
+        }
+        foreach (var character in _characters)
+        {
+            song.AddCharacter(character);
+        }
+        return song;
+    }
+}
+
