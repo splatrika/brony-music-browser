6637fd7ee3e9a9fa1393d969a85a26997fd75362
daniilbelov <tseflutty@yandex.ru>
2022-10-07 19:38:23 +0300

    Add read only repositories tests

---
+++ Server/Tests/Integration/Infrastructure/ReadOnlyAlbumRepositoryTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Splatrika.BronyMusicBrowser.Core.Interfaces;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data.Repositories.ReadOnly;
+using Splatrika.BronyMusicBrowser.Tests.Integration.Infrastructure.Builders;
+using Splatrika.BronyMusicBrowser.Tests.Unit.Core.Builders;
+using Xunit;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Integration.Infrastructure;
+
+public class ReadOnlyAlbumRepositoryTests : IDisposable
+{
+    private BrowserContext _context;
+    private IReadOnlyAlbumRepository _repository;
+
+
+    public ReadOnlyAlbumRepositoryTests()
+    {
+        _context = BrowserContextBuilder.BuildTesting();
+        _context.Database.EnsureDeleted();
+        _context.Database.Migrate();
+        _repository = new ReadOnlyAlbumRepository(_context);
+    }
+
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+
+    [Fact]
+    public async Task GetBySong()
+    {
+        var album1 = new AlbumBuilder()
+            .SetFirstSong(10)
+            .AddSong(12)
+            .Build();
+        var album2 = new AlbumBuilder()
+            .SetFirstSong(88)
+            .AddSong(12)
+            .Build();
+        var album3 = new AlbumBuilder()
+            .SetFirstSong(88)
+            .Build();
+        _context.AddRange(album1, album2, album3);
+        _context.SaveChanges();
+
+        var found1 = await _repository.GetFirstForSong(88);
+        var found2 = await _repository.GetFirstForSong(100);
+        Assert.NotNull(found1);
+        Assert.Equal(2, found1!.Id);
+        Assert.Null(found2);
+    }
+
+
+    [Fact]
+    public async Task GetTitle()
+    {
+        var album1 = new AlbumBuilder()
+            .Build();
+        var album2 = new AlbumBuilder()
+            .SetTitle("Glimmer Time")
+            .Build();
+        _context.AddRange(album1, album2);
+        _context.SaveChanges();
+
+        var album = await _repository.GetTitle(id: 2);
+        Assert.Equal(2, album.Id);
+        Assert.Equal("Glimmer Time", album.Title);
+    }
+
+
+    [Fact]
+    public async Task GetById()
+    {
+        var album1 = new AlbumBuilder()
+            .Build();
+        var album2 = new AlbumBuilder()
+            .SetTitle("Glimmer Time")
+            .Build();
+        _context.AddRange(album1, album2);
+        _context.SaveChanges();
+
+        var album = await _repository.Get(id: 2);
+        Assert.Equal(2, album.Id);
+        Assert.Equal("Glimmer Time", album.Title);
+        Assert.Equal(1, album.Songs.Count);
+    }
+
+
+    [Fact]
+    public async Task GetAll()
+    {
+        var album1 = new AlbumBuilder().Build();
+        var album2 = new AlbumBuilder().Build();
+        var album3 = new AlbumBuilder().Build();
+        var album4 = new AlbumBuilder().Build();
+        var album5 = new AlbumBuilder().Build();
+        _context.AddRange(album1, album2, album3, album4, album5);
+        _context.SaveChanges();
+
+        var page1 = await _repository.GetAll(count: 3, offset: 0);
+        var page2 = await _repository.GetAll(count: 3, offset: 3);
+        Assert.Equal(3, page1.Count);
+        Assert.Equal(2, page2.Count);
+        Assert.True(page1.Select(x => x.Id)
+            .SequenceEqual(new int[] { 1, 2, 3 }));
+        Assert.True(page2.Select(x => x.Id)
+            .SequenceEqual(new int[] { 4, 5 }));
+    }
+}
+
---
+++ Server/Tests/Integration/Infrastructure/ReadOnlySongRepositoryTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using Splatrika.BronyMusicBrowser.Core;
+using Splatrika.BronyMusicBrowser.Core.Entities;
+using Splatrika.BronyMusicBrowser.Core.Interfaces;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data.Repositories.ReadOnly;
+using Splatrika.BronyMusicBrowser.Tests.Integration.Infrastructure.Builders;
+using Splatrika.BronyMusicBrowser.Tests.Unit.Core.Builders;
+using Xunit;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Integration.Infrastructure;
+
+public class ReadOnlySongRepositoryTests : IDisposable
+{
+    private BrowserContext _context;
+    private IReadOnlySongRepository _repository;
+    private SongFilters? _queryFilters;
+
+
+    public ReadOnlySongRepositoryTests()
+    {
+        _context = BrowserContextBuilder.BuildTesting();
+        _context.Database.EnsureDeleted();
+        _context.Database.Migrate();
+        _repository = new ReadOnlySongRepository(_context,
+            GetFakeFiltersProvider());
+    }
+
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+
+    [Fact]
+    public async Task GetShortInfo()
+    {
+        var song1 = new SongBuilder()
+            .SetTitle("Song0")
+            .Build();
+        var song2 = new SongBuilder()
+            .SetTitle("The Magic Flicker")
+            .AddArtist(1)
+            .AddArtist(2)
+            .Build();
+        var artist1 = new Artist(0, "Artist1");
+        var artist2 = new Artist(0, "Artist2");
+        _context.AddRange(artist1, artist2, song1, song2);
+        _context.SaveChanges();
+
+        var song = await _repository.GetShort(id: 2);
+        Assert.Equal("The Magic Flicker", song.Title);
+        Assert.True(song.Artists
+            .SequenceEqual(new[] { 1, 2 }));
+    }
+
+
+    [Fact]
+    public async Task GetByFilters()
+    {
+        var song1 = new SongBuilder().Build();
+        var song2 = new SongBuilder()
+            .SetTitle("Song101")
+            .SetYouTubeId("???")
+            .Build();
+        var song3 = new SongBuilder().Build();
+        var song4 = new SongBuilder()
+            .SetTitle("Song101")
+            .SetYouTubeId("???")
+            .Build();
+        var song5 = new SongBuilder()
+            .SetTitle("Song101")
+            .SetYouTubeId("???")
+            .Build();
+        var song6 = new SongBuilder().Build();
+        _context.AddRange(song1, song2, song3, song4, song5);
+        _context.SaveChanges();
+
+        var filters = new SongFilters();
+        var page1 = await _repository.GetByFilters(filters, 2, 0);
+        var page2 = await _repository.GetByFilters(filters, 2, 2);
+
+        Assert.Equal(_queryFilters, filters);
+        Assert.Equal(2, page1.Count);
+        Assert.Single(page2);
+        Assert.True(page1.Select(x => x.Id)
+            .SequenceEqual(new[] { 2, 4 }));
+        Assert.True(page2.Select(x => x.Id)
+            .SequenceEqual(new[] { 5 }));
+    }
+
+
+    private IFilterProcessorsProvider<Song, SongFilters>
+        GetFakeFiltersProvider()
+    {
+        var mock = new Mock<IFilterProcessorsProvider<Song, SongFilters>>();
+
+        var filter1Mock = new Mock<IFilterProcessor<Song, SongFilters>>();
+        var filter2Mock = new Mock<IFilterProcessor<Song, SongFilters>>();
+
+        filter1Mock.Setup(m => m.ApplyFilter(
+            It.IsAny<IQueryable<Song>>(),
+            It.IsAny<SongFilters>()))
+            .Callback<IQueryable<Song>, SongFilters>(
+                (query, filters) => _queryFilters = filters )
+            .Returns<IQueryable<Song>, SongFilters>(
+                (query, filters) => query.Where(e => e.Title == "Song101"));
+
+        filter2Mock.Setup(m => m.ApplyFilter(
+            It.IsAny<IQueryable<Song>>(),
+            It.IsAny<SongFilters>()))
+            .Returns<IQueryable<Song>, SongFilters>(
+                (query, filters) => query.Where(e => e.YouTubeId == "???"));
+
+        mock.Setup(m => m.Get())
+            .Returns(new[] { filter1Mock.Object, filter2Mock.Object });
+
+        return mock.Object;
+    }
+}
+
