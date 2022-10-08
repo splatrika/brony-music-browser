ae6bd84124b4db35c95a30a41c6ac9e65f7223df
daniilbelov <tseflutty@yandex.ru>
2022-10-08 12:05:43 +0300

    Add fulltext migration

---
+++ Server/Source/Infrastructure/Data/Migrations/20221008082951_AddFullText.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Splatrika.BronyMusicBrowser.Infrastructure.Data.Migrations
+{
+    public partial class AddFullText : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT CATALOG BrowserFTCat AS DEFAULT",
+                suppressTransaction: true);
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON dbo.Songs(Title) " +
+                     "KEY INDEX PK_Songs",
+                suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON dbo.Songs",
+                suppressTransaction: true);
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT CATALOG BrowserFTCat",
+                suppressTransaction: true);
+        }
+    }
+}
