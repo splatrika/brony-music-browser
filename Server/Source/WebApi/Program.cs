99ecd87d8ee13cfe99af10c903c25e758c22e579
daniilbelov <tseflutty@yandex.ru>
2022-10-08 12:07:56 +0300

    Update web api startup: run seed

--- Server/Source/WebApi/Program.cs
+++ Server/Source/WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Splatrika.BronyMusicBrowser.Infrastructure;
+using Splatrika.BronyMusicBrowser.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +10,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddBrowserContext(builder.Configuration);
+builder.Services.AddCrudRepositories();
+builder.Services.AddFilterProcessors();
+builder.Services.AddReadOnlyRepositories();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        await BrowserContextSeed.TrySeed(scope.ServiceProvider);
+    }
     app.UseSwagger();
     app.UseSwaggerUI();
 }
