702fa1aa7b59502926b554d8426c8827841d135c
splatrika <tseflutty@yandex.ru>
2022-10-12 15:58:49 +0300

    (server) Update genre info: add order

--- Server/Source/Core/Projections/GenreInfo.cs
+++ Server/Source/Core/Projections/GenreInfo.cs
@@ -6,10 +6,12 @@ namespace Splatrika.BronyMusicBrowser.Core.Projections;
 public class GenreInfo : EntityBase, IReadOnlyProjection
 {
     public string Caption { get; set; }
+    public int Order { get; set; }
 
     public GenreInfo(Genre original) : base(original.Id)
     {
         Caption = original.Caption;
+        Order = original.Order;
     }
 }
 
