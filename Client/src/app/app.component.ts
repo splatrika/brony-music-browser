093c221842aa8a963ce97324557cf89d914b9a8c
splatrika <tseflutty@yandex.ru>
2022-10-12 18:08:49 +0300

    (client) Update app component: add blocks

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { SongFilters } from './core/models/song-filters.model';
+import { SongListSpecification } from './songs-list/intrefaces/song-list-specification';
+import { AllSongsSpecification } from './songs-list/specifications/all-songs.specification';
+import { ByFiltersSpecification } from './songs-list/specifications/by-filters.specification';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BronyMusicBrowser';
+
+  songListSpecification: SongListSpecification = new AllSongsSpecification();
+
+  genresControl = new FormControl<number[]>([]) as FormControl<number[]>;
+  charactersControl = new FormControl<number[]>([]) as FormControl<number[]>;
+
+  onSearchSubmitted(filters: SongFilters) {
+    this.songListSpecification = new ByFiltersSpecification(filters);
+  }
 }
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormModule } from './search-form/search-form.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { SongsListModule } from './songs-list/songs-list.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SharedModule,
+    ReactiveFormsModule,
+    CoreModule,
+    SearchFormModule,
+    SongsListModule,
     AppRoutingModule
   ],
   providers: [],
