6b14d023c75d69a2ebf631aaf252f6cd706220f8
splatrika <tseflutty@yandex.ru>
2022-10-15 16:51:09 +0300

    (server) Add fake email sender

---
+++ Server/Source/WebAdmin/Services/FakeEmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Services;
+
+public class FakeEmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        Console.WriteLine(
+            $"Fake email sender to {email} ({subject}): {htmlMessage}");
+        return Task.CompletedTask;
+    }
+}
+
