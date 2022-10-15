574fa31cae7f5cb408874a3a573dd7cdd2e1a859
splatrika <tseflutty@yandex.ru>
2022-10-15 21:52:11 +0300

    (server) Add library operation requirement

---
+++ Server/Source/WebAdmin/Authorization/Operations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Authorization;
+
+public static class Operations
+{
+    public static readonly string Create = "Create";
+    public static readonly string Read = "Read";
+    public static readonly string Update = "Update";
+    public static readonly string Delete = "Delete";
+}
+
---
+++ Server/Source/WebAdmin/Authorization/Requirements/LibraryOperationRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Authorization.Requirements;
+
+public class LibraryOperationRequirement : IAuthorizationRequirement
+{
+    public string Operation { get; }
+
+
+    public LibraryOperationRequirement(string operation)
+    {
+        Operation = operation;
+    }
+}
+
