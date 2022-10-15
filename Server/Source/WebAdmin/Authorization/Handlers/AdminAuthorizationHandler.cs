e3787665eaca6f9dbc257f6b705db94a2a14d786
splatrika <tseflutty@yandex.ru>
2022-10-15 18:56:48 +0300

    (server) Add admin authorization handler

---
+++ Server/Source/WebAdmin/Authorization/Handlers/AdminAuthorizationHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Splatrika.BronyMusicBrowser.Core.Constrants;
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Authorization.Handlers;
+
+public class AdminAuthorizationHandler : IAuthorizationHandler
+{
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        if (context.User.IsInRole(Roles.Admin))
+        {
+            foreach (var requirement in context.Requirements)
+            {
+                context.Succeed(requirement);
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
+
---
+++ Server/Tests/Unit/WebAdmin/Authorization/Handlers/AdminAuthorizationHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Splatrika.BronyMusicBrowser.Core.Constrants;
+using Splatrika.BronyMusicBrowser.WebAdmin.Authorization.Handlers;
+
+namespace Splatrika.BronyMusicBrowser.Tests.Unit.WebAdmin.Authorization.Handlers;
+
+public class AdminAuthorizationHandlerTests
+{
+    private AdminAuthorizationHandler _handler;
+    private IAuthorizationRequirement _requirement;
+
+    public AdminAuthorizationHandlerTests()
+    {
+        _handler = new();
+        _requirement = new Mock<IAuthorizationRequirement>().Object;
+    }
+
+    [Fact]
+    public async Task AuthorizeAdminToResource()
+    {
+        var user = new ClaimsPrincipal();
+        var adminRole = new Claim(ClaimTypes.Role, Roles.Admin);
+        user.AddIdentity(new(claims: new[] { adminRole }));
+
+        var context = new AuthorizationHandlerContext(
+            new[] { _requirement }, user, null);
+
+        await _handler.HandleAsync(context);
+
+        Assert.True(context.HasSucceeded);
+    }
+
+
+    [Fact]
+    public async Task AuthorizeUserWithoutRolesToResource()
+    {
+        var user = new ClaimsPrincipal();
+
+        var context = new AuthorizationHandlerContext(
+            new[] { _requirement }, user, null);
+
+        await _handler.HandleAsync(context);
+
+        Assert.False(context.HasSucceeded);
+    }
+}
+
