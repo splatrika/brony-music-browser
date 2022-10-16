754ab31c622b72004b901cf0054378994f2c72ad
splatrika <tseflutty@yandex.ru>
2022-10-16 21:00:25 +0300

    (server) Add characters crud

---
+++ Server/Source/WebAdmin/Areas/Library/Controllers/CharactersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Splatrika.BronyMusicBrowser.Core.CreateArgs;
+using Splatrika.BronyMusicBrowser.Core.Entities;
+using Splatrika.BronyMusicBrowser.Core.Interfaces;
+using Splatrika.BronyMusicBrowser.WebAdmin.Attributes;
+using Splatrika.BronyMusicBrowser.WebAdmin.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Areas.Library.Controllers;
+
+[Area("Library")]
+[LibraryResourceController(resourceName: "Characters", order: 2)]
+public class CharactersController : LibraryControllerBase
+    <Character, CharacterCreateArgs,
+    EditCharacterViewModel, EditCharacterViewModel>
+{
+    public CharactersController(
+        ICrudRepository<Character, CharacterCreateArgs> repository,
+        IAuthorizationService authorizationService)
+        : base(repository, authorizationService)
+    {
+    }
+
+
+    public override Task<Character> Create(
+        EditCharacterViewModel model,
+        ICrudRepository<Character, CharacterCreateArgs> repository)
+    {
+        return repository.Create(new(model.Name, model.Icon, model.Order));
+    }
+
+
+    public override void Edit(EditCharacterViewModel model, Character resource)
+    {
+        resource.UpdateDetails(model.Name, model.Icon, model.Order);
+    }
+
+
+    public override EditCharacterViewModel GetEditViewModel(Character resource)
+    {
+        return new EditCharacterViewModel
+        {
+            Name = resource.Name,
+            Icon = resource.Icon,
+            Order = resource.Order
+        };
+    }
+}
+
---
+++ Server/Source/WebAdmin/ViewModels/EditCharacterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.ViewModels;
+
+#nullable disable
+
+public class EditCharacterViewModel
+{
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; init; }
+
+    [Required]
+    [MaxLength(500)]
+    public string Icon { get; init; }
+
+    [Required]
+    public int Order { get; init; }
+}
+
