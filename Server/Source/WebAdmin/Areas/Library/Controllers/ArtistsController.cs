289301681594966d15607b56b645195ded755e18
splatrika <tseflutty@yandex.ru>
2022-10-16 20:59:53 +0300

    (server) Add artists crud

---
+++ Server/Source/WebAdmin/Areas/Library/Controllers/ArtistsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Splatrika.BronyMusicBrowser.Core.CreateArgs;
+using Splatrika.BronyMusicBrowser.Core.Entities;
+using Splatrika.BronyMusicBrowser.Core.Interfaces;
+using Splatrika.BronyMusicBrowser.WebAdmin.Attributes;
+using Splatrika.BronyMusicBrowser.WebAdmin.Authorization;
+using Splatrika.BronyMusicBrowser.WebAdmin.Authorization.Requirements;
+using Splatrika.BronyMusicBrowser.WebAdmin.ViewModels;
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Areas.Library.Controllers;
+
+[Area("Library")]
+[LibraryResourceController(resourceName: "Artists", order: 1)]
+public class ArtistsController : LibraryControllerBase
+    <Artist, ArtistCreateArgs, EditArtistViewModel, EditArtistViewModel>
+{
+    public ArtistsController(
+        ICrudRepository<Artist,
+        ArtistCreateArgs> repository,
+        IAuthorizationService authorizationService)
+        : base(repository, authorizationService)
+    {
+    }
+
+
+    public override Task<Artist> Create(EditArtistViewModel model,
+        ICrudRepository<Artist, ArtistCreateArgs> repository)
+    {
+        return repository.Create(new(model.Name));
+    }
+
+
+    public override void Edit(EditArtistViewModel model, Artist resource)
+    {
+        resource.UpdateDetails(model.Name);
+    }
+
+
+    public override EditArtistViewModel GetEditViewModel(Artist resource)
+    {
+        return new EditArtistViewModel
+        {
+            Name = resource.Name
+        };
+    }
+}
+
---
+++ Server/Source/WebAdmin/ViewModels/EditArtistViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Splatrika.BronyMusicBrowser.WebAdmin.ViewModels;
+
+#nullable disable
+
+public class EditArtistViewModel
+{
+    [Required]
+    public string Name { get; set; }
+}
+
