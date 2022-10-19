241fd3710ecf72d0a8d2995a83d397896886e421
splatrika <tseflutty@yandex.ru>
2022-10-19 20:32:30 +0300

    (server) Add song edit tab controller attribute

---
+++ Server/Source/WebAdmin/Attributes/SongEditTabControllerAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Splatrika.BronyMusicBrowser.WebAdmin.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SongEditTabControllerAttribute : Attribute
+{
+    public string TabName { get; }
+    public int Order { get; }
+    public string ActionName { get; }
+
+
+    public SongEditTabControllerAttribute(string tabName, int order = 0,
+        string actionName = "Index")
+    {
+        TabName = tabName;
+        Order = order;
+        ActionName = actionName;
+    }
+}
+
